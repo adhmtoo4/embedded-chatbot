dad24422efafd17046439504261f8f61206bb951
vitalii_polishchuk_dev <vitalii.polishchuk@appsdelivered.com>
2023-12-04 08:43:30 +0000

    React + Typescript installation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="App-header">Embedded chatbot</h2>
+      <button onClick={() => console.log('button clicked')}>Button</button>
+    </div>
+  );
+}
+
+export default App;
