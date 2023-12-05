bafff23e9fa302312b6b20faee70bea50a39b765
houssem.masri <houssem.masri@appsdelivered.com>
2023-12-05 15:01:56 +0100

    Fix update html

--- update-html.js
+++ update-html.js
@@ -1,13 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 
-// Read html_example.html
-const htmlPath = path.join(__dirname, 'html_example.html');
-let htmlContent = fs.readFileSync(htmlPath, 'utf8');
+// Define the path to your html_example.html file and your build directory
+const htmlFilePath = path.join(__dirname, 'html_example.html');
+const buildDirPath = path.join(__dirname, 'build', 'static');
 
-// Update CSS and JS links
-htmlContent = htmlContent.replace(/main\.[a-z0-9]+\.css/g, 'main.NEW_VERSION.css');
-htmlContent = htmlContent.replace(/main\.[a-z0-9]+\.js/g, 'main.NEW_VERSION.js');
+// Function to get the latest version numbers from the build directory
+function getLatestVersionNumbers() {
+    const cssFiles = fs.readdirSync(path.join(buildDirPath, 'css'));
+    const jsFiles = fs.readdirSync(path.join(buildDirPath, 'js'));
+
+    // Assuming filenames are like 'main.VERSION.css' or 'main.VERSION.js'
+    const cssVersion = cssFiles.find(file => file.startsWith('main.') && file.endsWith('.css')).split('.')[1];
+    const jsVersion = jsFiles.find(file => file.startsWith('main.') && file.endsWith('.js')).split('.')[1];
+
+    return { cssVersion, jsVersion };
+}
+
+// Read the HTML file
+let htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
+
+// Get the latest version numbers
+const { cssVersion, jsVersion } = getLatestVersionNumbers();
+
+// Replace the version numbers in the HTML file
+htmlContent = htmlContent.replace(/main\.[a-z0-9]+\.css/, `main.${cssVersion}.css`);
+htmlContent = htmlContent.replace(/main\.[a-z0-9]+\.js/, `main.${jsVersion}.js`);
 
 // Write the updated content back to html_example.html
-fs.writeFileSync(htmlPath, htmlContent);
+fs.writeFileSync(htmlFilePath, htmlContent);
